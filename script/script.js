b07e73e32e473f8c8252ac34aa132f5c4eddf003
Lefasto <g.palfoeldi@gmail.com>
2025-04-05 22:36:35 +0200

    add func for computerchoice and global variables

--- script/script.js
+++ script/script.js
@@ -1,1 +1,27 @@
-console.log("Hello World!");
+let humanScore = 0;
+let computerScore = 0;
+
+
+function getComputerChoice(){
+    let choice = parseInt(Math.random() * 3);
+
+    switch(choice){
+        case 0:
+            return "Rock";
+            break;
+        case 1:
+            return "Paper";
+            break;
+        case 2:
+            return "Scissor";
+            break;
+        default:
+            break;
+    }
+}
+
+function getHumanChoice(){
+    //TODO
+}
+
+console.log(getComputerChoice());
